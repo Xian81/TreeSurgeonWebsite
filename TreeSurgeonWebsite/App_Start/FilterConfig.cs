c39c74d300d4334f695a67c594c84ed85a2e3f20
Christian Howard <christian.howard@hotmail.co.uk>
2019-03-28 19:50:52 +0000

    Add project files.

---
+++ TreeSurgeonWebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TreeSurgeonWebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
