64637db5ce0053fcda5a476743e712c2eb1e284e
Christian Howard <christian.howard@hotmail.co.uk>
2019-03-29 14:32:36 +0000

    new dao

---
+++ TreeSurgeonWebsite.Data/IDAO/IContact.cs
@@ -0,0 +1,15 @@
+﻿using TreeSurgeonWebsite.Data; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeSurgeonWebsite.Data.IDAO
+{
+  public interface IContact
+    {
+
+        //IList<Contact> GetContacts();
+    }
+}
